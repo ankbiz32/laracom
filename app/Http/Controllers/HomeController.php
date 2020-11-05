8e12bc04566075e82f5b2dbd9b0ebcb6f17f102b
= <developer@cluebixsoftware.com>
2020-11-05 18:00:37 +0530

    Country wise product fetching

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends Controller
     {
         // dd(geoip($request->ip())); //For dynamic ip address
         // dd(geoip('178.18.25.0')); //For static ip address
+        // dd($products = Product::where('country_iso_code',geoip($request->ip())->iso_code)->take(4)->get());
         $products = Product::take(4)->get();
         return view('home.index',compact('products'));
 
--- database/migrations/2020_10_07_104409_create_payment_table.php
+++ database/migrations/2020_10_07_104409_create_payment_table.php
@@ -16,10 +16,12 @@ class CreatePaymentTable extends Migration
         Schema::create('payment', function (Blueprint $table) {
             $table->increments('id');
             $table->string('payment_amount');
+            $table->string('currency_code')->nullable();
             $table->string('vendor_order_id');
             $table->string('vendor_payment_id');
             $table->string('vendor_signature');
             $table->string('vendor_errors');
+            $table->string('payment_status');
             $table->timestamps();
         });
     }
